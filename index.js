372c0ee637bec1d7cfe93fbf68a76a6b966767e0
mmparadinha <matheusmparadinha@gmail.com>
2022-09-01 14:41:54 -0300

    Add log-in participant

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { MongoClient } from 'mongodb';
 import dayjs from 'dayjs';
-import joi from 'joi';
+import Joi from 'joi';
 
 const app = express();
 app.use(cors());
@@ -16,12 +16,12 @@ const mongoClient = new MongoClient(process.env.MONGO_URI);
 let db;
 
 mongoClient.connect().then(() => {
-	db = mongoClient.db("projeto12");
+    db = mongoClient.db("projeto12");
 });
 
 
 //Get participants and messages
-app.get('/participants', async function(req, res) {
+app.get('/participants', async function (req, res) {
     try {
         const participants = await db.collection('participants').find().toArray();
         res.send(participants);
@@ -30,7 +30,7 @@ app.get('/participants', async function(req, res) {
     }
 });
 
-app.get('/messages', async function(req, res) {
+app.get('/messages', async function (req, res) {
     try {
         const messages = await db.collection('messages').find().toArray();
         res.send(messages);
@@ -40,18 +40,21 @@ app.get('/messages', async function(req, res) {
 });
 
 //Post participants and messages
-app.post('/participants', async function(req, res) {
-    const { name } = req.body;
+app.post('/participants', async function (req, res) {
+    const schema = Joi.string().min(1);
+    const { value, error } = schema.validate(req.body.name);
+    const loggedIn = await db.collection('participants').find({name: `${value}`}).toArray();
 
-    
+    if (loggedIn.length !== 0) return res.status(409).send('Name already in use');
+    if (error) return res.status(422).send(error.details[0].message);
 
     try {
-        await db.collection('participants').insertOne({
-            name: name,
+        db.collection('participants').insertOne({
+            name: value,
             lastStatus: Date.now()
         })
-        await db.collection('messages').insertOne({
-            from: name,
+        db.collection('messages').insertOne({
+            from: value,
             to: 'Todos',
             text: 'entra na sala...',
             type: 'status',
@@ -60,7 +63,7 @@ app.post('/participants', async function(req, res) {
         res.sendStatus(201);
     } catch (error) {
         console.error(error);
-    }
+    } 
 });
 
 
